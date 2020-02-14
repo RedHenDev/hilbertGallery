98e937c158b9190a123c73a17b6bce0dfaf742d4
Brian New <neobn@hotmail.com>
2020-02-14 23:30:03 +0000

    Surely correct path now?
    https://redhendev.github.io/hilbertGallery/images/...

--- hilbertGallery.js
+++ hilbertGallery.js
@@ -13,7 +13,7 @@ function setup(){
 }
 
 function newTex(){
-	tex = loadImage("https://github.io/RedHenDev/hilbertGallery/images/hil" + 
+	tex = loadImage("https://redhendev.github.io/hilbertGallery/images/hil" + 
 					 int(random(1,16)) + ".png");
 }
 
