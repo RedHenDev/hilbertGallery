7854f4daa5d0bf43089a20a8e4cb5ec4a6124061
Brian New <neobn@hotmail.com>
2020-02-14 23:06:56 +0000

    Just dragged these up here

---
+++ hilbertGallery.js
@@ -0,0 +1,42 @@
+// Current texture applied to cube.
+let tex;
+
+function setup(){
+    
+   createCanvas(window.innerWidth,
+				window.innerWidth,
+			   WEBGL);
+	
+	newTex();
+	
+	strokeWeight(5);
+}
+
+function newTex(){
+	tex = loadImage("/images/hil" + 
+					 int(random(1,16)) + ".png");
+}
+
+function mousePressed(){
+	newTex();
+}
+
+function draw(){
+	background(101,
+			  0,
+			  101);
+	
+	speed = map(mouseX, 0, width,
+			   0.01, 12);
+	speedY = map(mouseY, 0, height,
+			   0.01, 12);
+	
+	translate(0,-width/7);
+	rotateY(speed);
+	rotateX(speedY);
+	texture(tex);
+	box(width/3.2);	
+	
+	if (frameCount % 200 === 0)
+		newTex();
+}
